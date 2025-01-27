d85673da18a797b9311cff2940252226601c5a82
Rustam <189756814+rustleium@users.noreply.github.com>
2025-01-27 11:27:45 +0530

    feat: implement divide() function and test it

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ function subtract(a, b) {
 function multiply(a, b) {
   return a * b;
 }
+
+function divide(a, b) {
+
+  if (b === 0) {
+    return 'Error: Cannot divide a number by 0';
+  } else {
+    return a / b;
+  }
+}
